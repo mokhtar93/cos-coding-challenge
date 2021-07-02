41b3025a9c13d55381d831e1a043383edf1cc8a1
ahmed <ahmed@vaangroup.com>
2021-07-02 02:16:51 +0200

    edited app component

--- challenges/web-frontend/src/app/app.module.ts
+++ challenges/web-frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from '../app/core/core.module';
+import {SharedModule} from '../app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
