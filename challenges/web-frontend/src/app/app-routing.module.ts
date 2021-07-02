e7ebc430bf645dd7bff4457926e71c09d230142b
ahmed <ahmed@vaangroup.com>
2021-07-02 02:16:01 +0200

    added routes

--- challenges/web-frontend/src/app/app-routing.module.ts
+++ challenges/web-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from '../app/core/home/home.component';
+import { LoginComponent } from '../app/core/login/login.component';
+import { AuthGuard } from '../app/core/guards/auth.guard';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
