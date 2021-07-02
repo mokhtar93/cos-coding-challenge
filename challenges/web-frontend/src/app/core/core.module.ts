526bdafd5591b3d4e1b9886deea6a51c0ac2d9a3
ahmed <ahmed@vaangroup.com>
2021-07-02 02:15:51 +0200

    edited ciore module

--- challenges/web-frontend/src/app/core/core.module.ts
+++ challenges/web-frontend/src/app/core/core.module.ts
@@ -2,16 +2,38 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {  ErrorInterceptor } from './interceptors/error.interceptor';
+import {  JwtInterceptor } from './interceptors/jwt.interceptor';
+import { CardComponent } from './card/card.component';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    CardComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SharedModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+],
+exports : [
+  HeaderComponent
+]
+
 })
 export class CoreModule { }
